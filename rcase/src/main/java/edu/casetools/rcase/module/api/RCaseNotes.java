3c92856cd75d14070150577b0e004304380f52b1
Unai <u.alegre@mdx.ac.uk>
2018-04-09 12:37:59 +0100

    Added argumentation tools

--- rcase/src/main/java/edu/casetools/rcase/module/api/RCaseNotes.java
+++ rcase/src/main/java/edu/casetools/rcase/module/api/RCaseNotes.java
@@ -2,7 +2,10 @@ package edu.casetools.rcase.module.api;
 
 public class RCaseNotes {
 
-	 public static final String NOTE_RATIONALE = "RationaleNote";
-	 public static final String NOTE_PROBLEM   = "ProblemNote";
-	
+	 public static final String NOTE_RATIONALE 		= "RationaleNote";
+	 public static final String NOTE_PROBLEM   		= "ProblemNote";
+	 public static final String NOTE_ARGUMENTATION 	= "ArgumentationNote";
+	 public static final String NOTE_SUPPORT 		= "SupportNote";
+	 public static final String NOTE_DENY 			= "DenyNote";	
+	 
 }
