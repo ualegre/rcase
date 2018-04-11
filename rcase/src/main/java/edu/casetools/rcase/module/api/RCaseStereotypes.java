937a9807e288060a2e53d057c81766ee228d9610
Unai <u.alegre@mdx.ac.uk>
2018-04-11 22:07:45 +0100

    Minor refactoring in stereotypes

--- rcase/src/main/java/edu/casetools/rcase/module/api/RCaseStereotypes.java
+++ rcase/src/main/java/edu/casetools/rcase/module/api/RCaseStereotypes.java
@@ -27,72 +27,70 @@ package edu.casetools.rcase.module.api;
 public class RCaseStereotypes {
 
     public static final String STEREOTYPE_DIAGRAM_USECASE = "UseCaseDiagram"; 
-    public static final String STEREOTYPE_USECASE = "UseCaseStereotype";
+    public static final String STEREOTYPE_USECASE 		  = "UseCaseStereotype";
     
 	public static final String STEREOTYPE_DIAGRAM_STAKEHOLDER = "StakeholderDiagramStereotype";
-	public static final String STEREOTYPE_STAKEHOLDER = "StakeholderStereotype";
-	public static final String STEREOTYPE_USER_PROFILE = "UserProfileStereotype";
-	public static final String STEREOTYPE_VALUE = "ValueStereotype";
-	public static final String STEREOTYPE_VALUE_ENHANCER = "ValueEnhancerStereotype";
-	public static final String STEREOTYPE_PARTICIPATION = "ParticipationStereotype";
-	public static final String STEREOTYPE_GRID = "GridStereotype";
-	public static final String STEREOTYPE_ETHICAL_PROFILE = "EthicalProfileStereotype";
-	public static final String STEREOTYPE_INTERESTED_IN = "InterestedInStereotype";
-	public static final String STEREOTYPE_REQUEST = "RequestStereotype";
-	public static final String STEREOTYPE_PROVIDE = "ProvideStereotype";
-	public static final String STEREOTYPE_HAS = "HasStereotype";
-	public static final String STEREOTYPE_INFLUENCE = "InfluenceStereotype";
-	public static final String STEREOTYPE_ENHANCE = "EnhanceStereotype";
+	public static final String STEREOTYPE_STAKEHOLDER 		  = "StakeholderStereotype";
+	public static final String STEREOTYPE_USER_PROFILE 		  = "UserProfileStereotype";
+	public static final String STEREOTYPE_VALUE 			  = "ValueStereotype";
+	public static final String STEREOTYPE_VALUE_ENHANCER 	  = "ValueEnhancerStereotype";
+	public static final String STEREOTYPE_PARTICIPATION 	  = "ParticipationStereotype";
+	public static final String STEREOTYPE_GRID 				  = "GridStereotype";
+	public static final String STEREOTYPE_ETHICAL_PROFILE 	  = "EthicalProfileStereotype";
+	public static final String STEREOTYPE_INTERESTED_IN 	  = "InterestedInStereotype";
+	public static final String STEREOTYPE_REQUEST 			  = "RequestStereotype";
+	public static final String STEREOTYPE_PROVIDE 			  = "ProvideStereotype";
+	public static final String STEREOTYPE_HAS 				  = "HasStereotype";
+	public static final String STEREOTYPE_INFLUENCE 		  = "InfluenceStereotype";
+	public static final String STEREOTYPE_ENHANCE 			  = "EnhanceStereotype";
 	
-    public static final String STEREOTYPE_DIAGRAM_REQUIREMENTS = "RequirementsDiagramStereotype";
-    public static final String STEREOTYPE_REQUIREMENT = "RequirementStereotype";
+    public static final String STEREOTYPE_DIAGRAM_REQUIREMENTS  = "RequirementsDiagramStereotype";
+    public static final String STEREOTYPE_REQUIREMENT 			= "RequirementStereotype";
     public static final String STEREOTYPE_REQUIREMENT_CONTAINER = "RequirementContainerStereotype";
-    public static final String STEREOTYPE_TESTCASE = "TestCaseStereotype";
-    public static final String STEREOTYPE_PART = "PartStereotype";
-    public static final String STEREOTYPE_COPY = "CopyStereotype";
-    public static final String STEREOTYPE_DERIVE = "DeriveStereotype";
-    public static final String STEREOTYPE_VERIFY = "VerifyStereotype";
-    public static final String STEREOTYPE_SATISFY = "SatisfyStereotype";
-    public static final String STEREOTYPE_REFINE = "RefineStereotype";   
-	public static final String STEREOTYPE_ARGUMENTATION = "ArgumentationStereotype";
+    public static final String STEREOTYPE_TESTCASE 				= "TestCaseStereotype";
+    public static final String STEREOTYPE_PART 					= "PartStereotype";
+    public static final String STEREOTYPE_COPY 					= "CopyStereotype";
+    public static final String STEREOTYPE_DERIVE 				= "DeriveStereotype";
+    public static final String STEREOTYPE_VERIFY 				= "VerifyStereotype";
+    public static final String STEREOTYPE_SATISFY 				= "SatisfyStereotype";
+    public static final String STEREOTYPE_REFINE 				= "RefineStereotype";   
+	public static final String STEREOTYPE_ARGUMENTATION 		= "ArgumentationStereotype";
 	
 	public static final String STEREOTYPE_DIAGRAM_OBJECTIVE = "ObjectiveDiagramStereotype";
-	public static final String STEREOTYPE_GOAL = "GoalStereotype";
-	public static final String STEREOTYPE_OBSTACLE = "ObstacleStereotype";
-	public static final String STEREOTYPE_RESOURCE = "ResourceStereotype";
-	public static final String STEREOTYPE_SOFT_GOAL = "SoftGoalStereotype";
-	public static final String STEREOTYPE_OBJECTIVE = "ObjectiveStereotype";
-	public static final String STEREOTYPE_HINDERS = "HindersStereotype";
-	public static final String STEREOTYPE_MITIGATES = "MitigatesStereotype";
-	public static final String STEREOTYPE_AND = "AndStereotype";	
-	public static final String STEREOTYPE_OR = "OrStereotype";	
-	public static final String STEREOTYPE_XOR = "XorStereotype";
-	public static final String STEREOTYPE_REFINEOBJ = "RefineObjStereotype";	
-	public static final String STEREOTYPE_CONTRIBUTION = "ContributesStereotype";
-	public static final String STEREOTYPE_MAKE = "MakeStereotype";
-	public static final String STEREOTYPE_HELP = "HelpStereotype";
-	public static final String STEREOTYPE_UNKNOWN = "UnknownStereotype";
-	public static final String STEREOTYPE_HURT = "HurtStereotype";
-	public static final String STEREOTYPE_BREAK = "BreakStereotype";
-	public static final String STEREOTYPE_EQUAL = "EqualStereotype";
+	public static final String STEREOTYPE_GOAL 				= "GoalStereotype";
+	public static final String STEREOTYPE_OBSTACLE 			= "ObstacleStereotype";
+	public static final String STEREOTYPE_RESOURCE 			= "ResourceStereotype";
+	public static final String STEREOTYPE_SOFT_GOAL 		= "SoftGoalStereotype";
+	public static final String STEREOTYPE_OBJECTIVE 		= "ObjectiveStereotype";
+	public static final String STEREOTYPE_HINDERS 			= "HindersStereotype";
+	public static final String STEREOTYPE_MITIGATES 		= "MitigatesStereotype";
+	public static final String STEREOTYPE_AND 				= "AndStereotype";	
+	public static final String STEREOTYPE_OR 				= "OrStereotype";	
+	public static final String STEREOTYPE_XOR 				= "XorStereotype";
+	public static final String STEREOTYPE_REFINEOBJ 		= "RefineObjStereotype";	
+	public static final String STEREOTYPE_CONTRIBUTION 		= "ContributesStereotype";
+	public static final String STEREOTYPE_MAKE 				= "MakeStereotype";
+	public static final String STEREOTYPE_HELP 				= "HelpStereotype";
+	public static final String STEREOTYPE_UNKNOWN 			= "UnknownStereotype";
+	public static final String STEREOTYPE_HURT 				= "HurtStereotype";
+	public static final String STEREOTYPE_BREAK 			= "BreakStereotype";
+	public static final String STEREOTYPE_EQUAL 			= "EqualStereotype";
 	
     public static final String STEREOTYPE_DIAGRAM_SITUATION_OF_INTEREST = "SituationOfInterestDiagramStereotype";  
-    public static final String STEREOTYPE_SITUATION_OF_INTEREST = "SituationOfInterestStereotype";
-	public static final String STEREOTYPE_CONTEXT_AWARE_FEATURE = "ContextAwareFeatureStereotype";
-    public static final String STEREOTYPE_TRIGGERS = "TriggersStereotype";
-    public static final String STEREOTYPE_ARISES   = 	"ArisesStereotype";
+    public static final String STEREOTYPE_SITUATION_OF_INTEREST 		= "SituationOfInterestStereotype";
+	public static final String STEREOTYPE_CONTEXT_AWARE_FEATURE 		= "ContextAwareFeatureStereotype";
+    public static final String STEREOTYPE_TRIGGERS 						= "TriggersStereotype";
+    public static final String STEREOTYPE_ARISES   						= 	"ArisesStereotype";
     
-    public static final String STEREOTYPE_DIAGRAM_CONTEXT = "ContextDiagramStereotype";
+    public static final String STEREOTYPE_DIAGRAM_CONTEXT 	= "ContextDiagramStereotype";
     public static final String STEREOTYPE_CONTEXT_ATTRIBUTE = "ContextAttributeStereotype";
     
-    public static final String STEREOTYPE_CONTEXT_DERIVE = "ContextDeriveStereotype";
+    public static final String STEREOTYPE_CONTEXT_DERIVE 	 = "ContextDeriveStereotype";
     public static final String STEREOTYPE_CONTEXT_DEPENDENCY = "ContextDependencyStereotype";
     public static final String STEREOTYPE_CONTEXT_IDENTIFIES = "IdentifiesStereotype";
 
     public static final String DEFAULT_STEREOTYPE = STEREOTYPE_COPY;
 	
-
-
     private RCaseStereotypes() {
 
     }
